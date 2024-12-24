1fd8cab5d075fbe6a565eb18360078ebe1dde4ed
BugHunter-X <askzenai@gmail.com>
2024-12-25 00:46:06 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,8 @@
+function printArray(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+let myArray: number[] = [1, 2, 3, 4, 5];
+printArray(myArray);
